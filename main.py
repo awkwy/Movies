0549e8b7e18c38aaeaa9d4d53014a0b1eb6f4c46
awkwardly yours <lionel.eole@gmail.com>
2026-02-08 22:40:29 +0300

    test api

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests 
 import yaml
 
-url = "https://api.themoviedb.org/3/authentication"
+url = "https://api.themoviedb.org/3"
 with open("config.yml",'r',encoding="utf-8") as file : 
     config = yaml.safe_load(file)
 
@@ -11,7 +11,42 @@ headers = {
     "Authorization": f"Bearer {Bearer}"
 }
 
+horror_movies = []
 
-response = requests.get(url, headers=headers)
+for page in range(1,6) : 
+    """_summary_
+    genres = 27 => horror
+    """
+    query_url = f"{url}/discover/movie?with_genres=27&sort_by=popularity.des&vote_count.gte=500=fr-FR&page={page}"
+    response = requests.get(query_url,headers=headers)
+    if response.status_code != 200:
+        print(f"Erreur lors de la requête page {page}")
+        break
 
-print(response.text)
+    data = response.json()
+
+    for movie in data['results']:
+        movie_id = movie['id']
+
+        img_url = f"{url}/movie/{movie_id}/images?include_image_language=fr,en"
+        img_res = requests.get(img_url, headers=headers).json()
+
+        posters = img_res.get("posters",[])
+
+        poster_fr = next((p['file_path'] for p in posters if p['iso_639_1'] == 'fr'),None)
+        poster_en = next((p['file_path'] for p in posters if p['iso_639_1'] == 'en'),None)
+
+        horror_movies.append({
+            "titre": movie['title'],
+            "rating": movie['vote_average'],
+            "synopsis": movie["overview"],
+            "affiche_fr": f"https://image.tmdb.org/t/p/w500{poster_fr}" if poster_fr else "Non disponible",
+            "affiche_en": f"https://image.tmdb.org/t/p/w500{poster_en}"
+             if poster_en else "Non disponible"
+        })
+
+if horror_movies:
+    print(f"\nRécupération terminée ! {len(horror_movies)} films enregistrés.")
+    print(f"Exemple : {horror_movies[0]['titre']}")
+    print(f"Affiche FR : {horror_movies[0]['affiche_fr']}")
+    print(f"Affiche EN : {horror_movies[0]['affiche_en']}")
